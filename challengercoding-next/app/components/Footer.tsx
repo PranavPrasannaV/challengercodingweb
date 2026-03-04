88427d4a5eaeff46006e75bd86cefda2e1376d08
pranavprasannav <pranavprasavenk@gmail.com>
2026-03-04 10:59:57 -0800

    hi

--- challengercoding-next/app/components/Footer.tsx
+++ challengercoding-next/app/components/Footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
                         </div>
                         <div className="flex items-center gap-3 text-gray-300">
                             <MapPin className="w-5 h-5 text-accent" />
-                            <span>San Jose, CA</span>
+                            <span>Sammamish, WA</span>
                         </div>
                     </div>
                 </div>
