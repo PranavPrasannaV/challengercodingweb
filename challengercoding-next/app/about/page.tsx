7d1a90c6d9464a441b872c7cb5e1e171c4aa18b8
aadisaraf <saraf.aadarsh@gmail.com>
2026-07-16 16:30:56 -0700

    updated roles

--- challengercoding-next/app/about/page.tsx
+++ challengercoding-next/app/about/page.tsx
@@ -11,8 +11,9 @@ const values = [
 
 const team = [
   { name: 'Aadi Saraf', role: 'Co-Founder' },
-  { name: 'Pranav Prasanna Venkatesh', role: 'VP of Education' },
+  { name: 'Pranav Prasanna Venkatesh', role: 'Vice President' },
   { name: 'Miheer Pandya', role: 'Webmaster' },
+  { name: 'Jeswanth Battula', role: 'Head of Public Relations' },
 ];
 
 export default function About() {
